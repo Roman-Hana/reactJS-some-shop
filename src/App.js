108e0424fb1bebd7a0ff19e6ee56278dec7c1978
Roman <romanhanaua@gmail.com>
2022-03-25 00:43:21 +0200

    created components and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import ProductListing from "./components/ProductListing";
+import ProductDetail from "./components/ProductDetail";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="App">
-     <h1>App</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
